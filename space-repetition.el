3fffc5a7644be16dfb10c8b23d8964fbb7644e4b
Hebi Li <lihebi.com@gmail.com>
2018-09-30 12:05:25 -0500

    init

---
+++ space-repetition.el
@@ -0,0 +1,15 @@
+;;; space-repetition.el --- A space repetition package. -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018 Hebi Li
+
+;; Author: Hebi Li <lihebi.com@gmail.com>
+;; Version: 0.1
+;; Keywords: Space Repetition
+;; URL: http://example.com/space-repetition
+
+;;; Commentary:
+;;; Code:
+
+
+(provide 'space-repetition)
+;;; space-repetition.el ends here
